d35888e9fd59d2e2ff6c8f5c9fcffb0fccf38e58
Michael Dobrinski <michael.git2015@gmail.com>
2015-11-20 08:29:54 -0600

    Basic function of program is working

--- BallDrop.java
+++ BallDrop.java
@@ -1,9 +1,47 @@
+import java.util.Scanner;
 
 public class BallDrop {
 
 	public static void main(String[] args) {
+		char direction = ' ';
+		int numOfBalls = 0, numSlots = 0, numLRdecisions = 0, slotNumber = 0;
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("\nEnter the number of slots in the game machine: ");
+		numSlots = input.nextInt();
+		System.out.print("\nEnter the number of balls to drop: ");
+		numOfBalls = input.nextInt();
+		int []  slots = new int[numSlots];
+		numLRdecisions = numSlots-1;
+
+		for (int i = 1; i<=numOfBalls; i++){
+			slotNumber = 0;
+			System.out.println();
+			for (int j = 1; j<=numLRdecisions; j++){
+				if (getDecision() =='R'){
+					slotNumber++;
+					System.out.print("R");
+				}
+				else{
+					System.out.print("L");
+				}
+				slots[slotNumber]++;
+			}
+
+		}
 
 
+	} // End of Main method
+
+	public static char getDecision(){
+		if (MyRandoms.randomNumber(1,2) == 1){
+			return 'L';
+		}
+		else {
+			return 'R';
+		}
 	}
 
-}
+
+
+} // End of BallDrop class
